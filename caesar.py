c3f5250f1f202885bebe0ddb87a2aa5fa7c6d8b7
Mickle <misha_2003_5@mail.ru>
2023-10-22 03:36:07 +0500

    Англофицировали шифр Цезаря

--- caesar.py
+++ caesar.py
@@ -1,4 +1,6 @@
 def caesar_encrypt(text, shift):
+    alphabet_lover_eng = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_upper_eng = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     alphabet_lover = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     alphabet_upper = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     result = ''
@@ -10,11 +12,17 @@ def caesar_encrypt(text, shift):
         elif char in alphabet_upper:
             index = (alphabet_upper.index(char) + shift) % len(alphabet_upper)
             result += alphabet_upper[index]
+        elif char in alphabet_lover_eng:
+            index = (alphabet_lover_eng.index(char) + shift) % len(alphabet_lover_eng)
+            result += alphabet_lover_eng[index]
+        elif char in alphabet_upper_eng:
+            index = (alphabet_upper_eng.index(char) + shift) % len(alphabet_upper_eng)
+            result += alphabet_upper_eng[index]
         else:
             result += char
     return result
 
-text = input('Введите строку (буквами русского алфавита): ')
+text = input('Введите строку (русскими или английскими буквами): ')
 while True:
     try:
         shift = int(input('Введите отступ (число): '))
